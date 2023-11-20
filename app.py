dcb2e75b30bc2d94be5023f6d6aafd34cf0af6e9
Kiran <kiransharma8685@gmail.com>
2023-11-20 16:40:23 +0530

    adding host port in app.py

--- app.py
+++ app.py
@@ -220,4 +220,5 @@ def permission_denied(e):
     return redirect('/')
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
